20b288fad7b60382156c91762b5338f68f16f1ee
Esben Viskum <viskum@viskum.dk>
2021-05-20 12:08:45 +0200

    Project prework complete

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,11 +6,186 @@
 //
 
 import SwiftUI
+import UserNotifications
+import SamplePackage
+
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+}
+
+class DelayedUpdater: ObservableObject {
+//    @Published var value = 0
+    
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
+}
 
 struct ContentView: View {
+//    @State private var selectedTab = "Tab1"
+    
+//    @ObservedObject var updater = DelayedUpdater()
+    
+//    @State private var backgroundColor = Color.red
+    
+    let possibleNumbers = Array(1...60)
+    
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ", ")
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        Text(results)
+        
+        
+/*        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+        } */
+
+        
+        
+        /*        VStack {
+            Text("Hello World")
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu {
+                    Button(action: {
+                        self.backgroundColor = .red
+                    }) {
+                        Text("Red")
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.red)
+                    }
+                    Button(action: {
+                        self.backgroundColor = .green
+                    }) {
+                        Text("Green")
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.green)
+                    }
+                    Button(action: {
+                        self.backgroundColor = .blue
+                    }) {
+                        Text("Blue")
+                        Image(systemName: "checkmark.circle.fill")
+                            .foregroundColor(.blue)
+                    }
+                }
+        } */
+        
+/*        Image("example")
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(Color.black)
+            .edgesIgnoringSafeArea(.all) */
+        
+//        Text("Value is: \(updater.value)")
+        
+        
+        /*        Text("Hello World")
+            .onAppear {
+                self.fetchData(from: "https:/www.apple.com") { result in
+                    switch result {
+                    case .success(let str):
+                        print(str)
+                    case .failure(let error):
+                        switch error {
+                        case .badURL:
+                            print("Bad URL")
+                        case .requestFailed:
+                            print("Network problem")
+                        case .unknown:
+                            print("Unknown error")
+                        }
+                    }
+                    
+                } */
+           /*     let url = URL(string: "https://www.apple.com")!
+                URLSession.shared.dataTask(with: url) { data, response, error in
+                    if data != nil {
+                        print("We got data")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }.resume() */
+//            }
+        
+/*        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    self.selectedTab = "Tab2"
+                }
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("One")
+                }
+                .tag("Tab1")
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+                }
+                .tag("Tab2")
+        } */
+    }
+    
+    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.badURL))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                if let data = data {
+                    let stringData = String(decoding: data, as: UTF8.self)
+                    completion(.success(stringData))
+                } else if error != nil {
+                    completion(.failure(.requestFailed))
+                } else {
+                    completion(.failure(.badURL))
+                }
+            }
+        }.resume()
     }
 }
 
