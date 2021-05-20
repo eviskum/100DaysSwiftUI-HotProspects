9b356dfdf7c49a8f838f2748f9cc46b160b3eb3d
Esben Viskum <viskum@viskum.dk>
2021-05-20 09:54:17 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Esben Viskum on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
